e789ed710916df2aebd93e70093606a5662a8259
JKrysztofiak <jakub.krysztofiak@protonmail.com>
2021-03-13 10:27:55 +0100

    BehaviorSubcect to Observable fixed

--- src/app/expenses.service.ts
+++ src/app/expenses.service.ts
@@ -16,13 +16,17 @@ export class ExpensesService {
   private expensesUrl = 'https://expense-backend.azurewebsites.net/accounts/6043f43843124e952d95073d/'
 
   expenses: Expense[] = [];
-  expenseSubject = new BehaviorSubject<Expense[]>([]);
+  expenseSubject = new BehaviorSubject<Expense[]>(this.expenses);
 
   constructor(private http: HttpClient) {
     this.getAccount().subscribe((acc) => {
       this.expenses = acc.expenses;
       this.update();
     })
+   }
+
+   get expensesList(){
+     return this.expenseSubject.asObservable();
    }
 
    getAccount(): Observable<Account> { 
@@ -40,10 +44,6 @@ export class ExpensesService {
        this.update();
      })
    } 
-
-  subscribe(observer: Observer<Expense[]>) {
-    this.expenseSubject.subscribe(observer);
-  }
 
   update() {
     this.expenseSubject.next(this.expenses);
--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ExpensesService } from '../expenses.service';
 import { Expense } from '../models/expense';
 import { Account } from '../models/account';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-feed',
@@ -13,11 +13,10 @@ export class FeedComponent implements OnInit {
 
   constructor(private expenseService: ExpensesService) { }
 
-  expenses = new BehaviorSubject<Expense[]>([]);
+  expenses = new Observable<Expense[]>();
 
   ngOnInit(): void {
-    this.expenseService.subscribe(this.expenses);
-    console.log(this.expenses);
+    this.expenses = this.expenseService.expensesList;
   }
 
 
