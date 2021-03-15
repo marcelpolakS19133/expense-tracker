931c97ae4cd4175beb7e68c5c4c7283ab01b4250
polmo <polmo@MAREK>
2021-03-15 01:09:49 +0100

    Create auth service

---
+++ src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Credentials} from './models/credentials';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private expensesUrl = 'https://expense-backend.azurewebsites.net/auth/';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  register(creds: Credentials): void {
+    this.httpClient.post(this.expensesUrl + 'register/', creds, this.httpOptions).subscribe(resp => console.log(resp));
+  }
+
+  login(creds: Credentials): void {
+    this.httpClient.post(this.expensesUrl + 'login', creds, this.httpOptions).subscribe(resp => console.log(resp));
+  }
+
+  logout(): void {
+    this.httpClient.post(this.expensesUrl + 'logout', {}, this.httpOptions).subscribe(resp => console.log(resp));
+  }
+}
